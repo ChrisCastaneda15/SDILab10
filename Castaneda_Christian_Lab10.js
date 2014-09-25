2cb31050e3e4a99bd1dbba8808d42118fe3ddb60
Chris Castaneda <SLEVEN15@fullsail.edu>
2014-09-25 14:53:01 -0400

    1st commit

---
+++ Castaneda_Christian_Lab10.js
@@ -0,0 +1,29 @@
+/*
+ * Christian Castaneda
+ * September 25th, 2014
+ * SDI
+ * Lab 10
+ */
+
+// Variables
+    
+// Problem one - Multiples of 3 and 5
+    
+    var threeNfive = function()
+    {
+        var threeSum = 0; //<---LOL
+        var fiveSum  = 0; //<---LOL
+        
+        //LOOP for 3
+        for(i = 0; i < 334; i++)
+        {
+            threeSum = threeSum + (3 * i);                
+        };
+        
+        //LOOP for 5
+        for(x = 0; x < 190; x++)
+        {
+            fiveSum = fiveSum + (5 * x);                
+        };
+          
+
